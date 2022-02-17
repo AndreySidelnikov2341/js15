e979bf074f1bcf3044e966b1e4bae6c786a67c74
AndreySidelnikov <andron2341991@gmail.com>
2022-02-17 23:15:57 +0200

    commit_js15

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+var myNum=10;
+var myStr='строка';
+var myBool=true;
+var myArr=[1,2,3,4,5];
+var myObj={first: 'First Name', last: 'Last Name'}
+myNum.toFixed(2);
+var decimal2=myNum.toFixed(2);
+console.log(decimal2);
+var i=5;
+console.log(i++);
+console.log(++i);
+console.log(i--);
+console.log(--i);
+var myTest=20;
+console.log(myTest+=myNum);
+console.log(myTest-=myNum);
+console.log(myTest*=myNum);
+console.log(myTest%=myNum);
+console.log(myTest/=myNum);
+console.log(Math.Pi);
+var Pi=3.141592653589793;
+var myPi=Pi;
+var round=89;
+var myRound=round;
+console.log(Math.round);
+var myRandom=Math.random()*10;
+var myPow=Math.pow(3,5);
+var strObj={str: "Мама мыла раму, рама мыла маму"};
+strObj.length=strObj.str.length;
+var isRamaPos=str;
+var isRamaPos=str.indexOf('рама');
+console.log(isRamaPos);
+var strReplace=strObj.str;
+var strReplace=strReplace.replace('Мама мыла раму, рама мыла маму', 'Мама моет раму, Рама держит маму');
+console.log(strReplace);
+var myUpper
+myStr.toUpperCase();
+myStr.toLowerCase();
